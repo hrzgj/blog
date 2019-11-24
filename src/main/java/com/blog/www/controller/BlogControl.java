f5e4b570931e000ceb3764b150aebf5bad19aff9
LYX708194 <1356250354@qq.com>
2019-11-24 23:37:06 +0800

    查询收藏夹列表，查询收藏夹的博客列表（包括默认和非默认收藏夹），查看单篇全部文章，修改发送验证码的漏洞，修改博客的收藏夹（默认至非默认）

--- src/main/java/com/blog/www/controller/BlogControl.java
+++ src/main/java/com/blog/www/controller/BlogControl.java
@@ -6,11 +6,13 @@ import com.blog.www.service.BlogService;
 import com.blog.www.service.CollectService;
 import com.blog.www.utils.CheckUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * @author lyx
