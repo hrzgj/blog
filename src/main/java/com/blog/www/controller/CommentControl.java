9644027fca0e68151c66cf88cf69a8282fae97f9
LYX708194 <1356250354@qq.com>
2020-07-25 11:59:31 +0800

    修改一些内容

--- src/main/java/com/blog/www/controller/CommentControl.java
+++ src/main/java/com/blog/www/controller/CommentControl.java
@@ -119,12 +119,12 @@ public class CommentControl {
             result.setCode(ResultCode.OBJECT_NULL);
             return result;
         }
-        User user= (User) request.getSession().getAttribute("user");
-        if(!comment.getCommenter().getId().equals(user.getId())){
-            result.setCode(ResultCode.USER_DIFFERENT);
-            result.setMsg("用户权限错误");
-            return result;
-        }
+//        User user= (User) request.getSession().getAttribute("user");
+//        if(!comment.getCommenter().getId().equals(user.getId())){
+//            result.setCode(ResultCode.USER_DIFFERENT);
+//            result.setMsg("用户权限错误");
+//            return result;
+//        }
         if(comService.deleteComment(comment)==1){
             result.setCode(ResultCode.SUCCESS);
             result.setMsg("删除成功");
